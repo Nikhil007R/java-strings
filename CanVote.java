3ef6e1f067dee6df4d154f2a6a1f7774f7258419
Nikhil Rawat <nikhilrawaa311@gmail.com>
2025-03-06 12:58:53 +0530

    [Nikhil] Add: Check voting eligibility for students, handle invalid ages

---
+++ CanVote.java
@@ -0,0 +1,64 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class StudentVotingEligibility {
+
+    // Method to generate random ages for students
+    public static int[] generateAges(int n) {
+        int[] ages = new int[n];
+        Random rand = new Random();
+
+        for (int i = 0; i < n; i++) {
+            ages[i] = rand.nextInt(90) - 10; // Random age between -10 and 80
+        }
+
+        return ages;
+    }
+
+    // Method to check voting eligibility
+    public static String[][] checkVotingEligibility(int[] ages) {
+        String[][] result = new String[ages.length][2];
+
+        for (int i = 0; i < ages.length; i++) {
+            result[i][0] = String.valueOf(ages[i]);
+
+            if (ages[i] < 0) {
+                result[i][1] = "Invalid Age";
+            } else if (ages[i] >= 18) {
+                result[i][1] = "Can Vote";
+            } else {
+                result[i][1] = "Cannot Vote";
+            }
+        }
+
+        return result;
+    }
+
+    // Method to display the 2D array in tabular format
+    public static void displayTable(String[][] data) {
+        System.out.println("\nStudent Age\tVoting Eligibility");
+        System.out.println("---------------------------------");
+
+        for (String[] row : data) {
+            System.out.println(row[0] + "\t\t" + row[1]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking user input for number of students
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+
+        // Generate random ages
+        int[] studentAges = generateAges(n);
+
+        // Check voting eligibility
+        String[][] eligibilityTable = checkVotingEligibility(studentAges);
+
+        // Display results in a table
+        displayTable(eligibilityTable);
+
+    }
+}
