695779d85fc849eb1f9112d01b3b88566da517df
Nikhil Rawat <nikhilrawaa311@gmail.com>
2025-03-06 10:00:36 +0530

    [Nikhil] Add: comparing two string with charAt and equals.

---
+++ CompareStrings.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class CompareStrings {
+
+    public static boolean isEqual(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input from user
+        System.out.println("Enter first String: ");
+        String str1 = sc.next();
+        System.out.println("Enter second String: ");
+        String str2 = sc.next();
+
+        // calling the method
+        boolean result = isEqual(str1, str2);
+
+        System.out.println("After comparing two strings with charAt()");
+        System.out.println(result);
+        System.out.println("After comparing two strings with .equals()");
+        System.out.println(str1.equals(str2));
+
+    }
+}
